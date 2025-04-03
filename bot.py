f4e94a85e5ff1a63b93c2c15c4153b9911332f9a
Федосеева Николетта <nikoletta@tirscript.com>
2025-04-03 13:25:22 +0300

    up

--- bot.py
+++ bot.py
@@ -1,46 +1,30 @@
 import os
-import instaloader
-import schedule
-import time
-from telegram import Bot
+from telegram import Update
+from telegram.ext import Application, CommandHandler
 from dotenv import load_dotenv
 
 # Загружаем переменные окружения
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
 
-bot = Bot(token=BOT_TOKEN)
+# Функция для команды /start
+async def start(update: Update, context):
+    print("Вход в функцию start")
+    # Отправка картинки по ссылке
+    image_url = "https://www.imgonline.com.ua/examples/bee-on-daisy.jpg"
+    await context.bot.send_photo(chat_id=update.message.chat_id, photo=image_url, caption="Привет! Вот картинка, как ты и просил!")
 
+# Основная функция для запуска бота
+def main():
+    print("Вход в функцию main")
+    application = Application.builder().token(BOT_TOKEN).build()
+    # Обработка команды /start
+    application.add_handler(CommandHandler("start", start))
 
-# Функция для скачивания истории
-def download_story():
-    loader = instaloader.Instaloader(download_pictures=True, quiet=True)
-    stories = loader.get_stories()
-
-    for story in stories:
-        if story.owner_username.lower() == INSTAGRAM_USERNAME.lower():
-            for item in story.get_items():
-                loader.download_storyitem(item, "stories")
-                file_path = f"stories/{item.mediaid}.jpg"
-                return file_path
-    return None
-
-
-# Функция отправки истории в Telegram
-def send_story():
-    image_path = download_story()
-    if image_path:
-        bot.send_photo(chat_id="@your_channel", photo=open(image_path, "rb"), caption="Новая история!")
-    else:
-        bot.send_message(chat_id="@your_channel", text="Историй нет.")
-
-
-# Автоматический запуск в 07:00 UTC
-schedule.every().day.at("07:00").do(send_story)
+    print("Вход в функцию main application ", application)
+    # Запуск бота
+    application.run_polling()
 
 if __name__ == "__main__":
     print("Бот запущен 🚀")
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    main()
